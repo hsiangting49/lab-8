65858adc09158a9528926836e6d9db786e901498
hsiangting49 <157249687+hsiangting49@users.noreply.github.com>
2024-03-18 14:59:36 +0000

    v2

--- utilities.js
+++ utilities.js
@@ -4,7 +4,8 @@
 const supabaseUrl = 'https://wqtvclmwjxsvcwhacqoo.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxdHZjbG13anhzdmN3aGFjcW9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA2NDU1MzcsImV4cCI6MjAyNjIyMTUzN30.Je5o6-W7MaOTYUezxqBIZEZdJlA-Kx-s9WZfSi2Ec0k'
 const supabase = createClient(supabaseUrl, supabaseKey)
-*/
+
+import { createClient } from '@supabase/supabase-js'
 async function getbooks(){
   let { data: books, error } = await supabase
   .from('books')
@@ -18,3 +19,4 @@ async function getbooks(){
 
 getbooks();
 
+*/
