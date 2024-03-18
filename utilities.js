f79ed9d377b377eca85b5967160c8621f626e5b9
hsiangting49 <157249687+hsiangting49@users.noreply.github.com>
2024-03-18 15:29:38 +0000

    v2

--- utilities.js
+++ utilities.js
@@ -19,4 +19,38 @@ async function getbooks(){
 
 getbooks();
 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fav-Books</title>
+    <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+    <script src="utilities.js"></script>
+</head>
+<body>
+    <table border="1" id="books">
+    </table>
+    <script>
+        const { createClient } = supabase;
+        const supabaseUrl = 'https://wqtvclmwjxsvcwhacqoo.supabase.co';
+        const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxdHZjbG13anhzdmN3aGFjcW9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA2NDU1MzcsImV4cCI6MjAyNjIyMTUzN30.Je5o6-W7MaOTYUezxqBIZEZdJlA-Kx-s9WZfSi2Ec0k';
+        const supabase = createClient(supabaseUrl, supabaseKey);
+        async function getbooks(){
+            let { data: books, error } = await supabase
+            .from('books')
+            .select('*')
+
+            for (let book of books) {
+                let booklist = document.getElementById('books');
+                booklist.innerHTML += `<td>${book.title}</td> <td>${book.author}</td> <td>${book.isbn}</td>`;
+            }
+            };
+
+            getbooks();
+    </script>
+</body>
+</html>
+
 */
