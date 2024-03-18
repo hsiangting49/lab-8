bb3d050cb72636da606ac5d1ba58a4e8b7891f00
hsiangting49 <157249687+hsiangting49@users.noreply.github.com>
2024-03-18 14:34:57 +0000

    v2

--- utilities.js
+++ utilities.js
@@ -1,6 +1,6 @@
 /*import { createClient } from '@supabase/supabase-js'*/
 /*"https://esm.sh/@supabase/supabase-js"*/
-const {createClient} = @supabase/supabase-js
+const {createClient} = supabase
 const supabaseUrl = 'https://wqtvclmwjxsvcwhacqoo.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxdHZjbG13anhzdmN3aGFjcW9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA2NDU1MzcsImV4cCI6MjAyNjIyMTUzN30.Je5o6-W7MaOTYUezxqBIZEZdJlA-Kx-s9WZfSi2Ec0k'
 const supabase = createClient(supabaseUrl, supabaseKey)
